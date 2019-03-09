32986b7901ff3f53ed28b1a224781ad9e1dc971b
Dhanush-raj <adhanushrj@gmail.com>
2019-03-09 13:52:05 +0530

    [PATCH] HID keyboard/mouse gadget support

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -62,6 +62,9 @@
 #include "f_ccid.c"
 #include "f_mtp.c"
 #include "f_accessory.c"
+#include "f_hid.h"
+#include "f_hid_android_keyboard.c"
+#include "f_hid_android_mouse.c"
 #include "f_rndis.c"
 #include "rndis.c"
 #include "f_qc_ecm.c"
@@ -72,7 +75,6 @@
 #include "f_ecm.c"
 #include "u_ether.c"
 #include "u_qc_ether.c"
-#include "f_hid.c"
 #ifdef CONFIG_TARGET_CORE
 #endif
 #ifdef CONFIG_SND_PCM
@@ -1427,29 +1429,6 @@ static struct android_usb_function audio_function = {
 };
 #endif
 
-static int hid_function_init(struct android_usb_function *f,
-				 struct usb_composite_dev *cdev)
-{
-	return ghid_setup(cdev->gadget, 2);
-}
-
-static void hid_function_cleanup(struct android_usb_function *f)
-{
-	ghid_cleanup();
-}
-
-static int hid_function_bind_config(struct android_usb_function *f,
-					struct usb_configuration *c)
-{
-	return hidg_bind_config(c, NULL, 0);
-}
-
-static struct android_usb_function hid_function = {
-	.name		= "hid",
-	.init		= hid_function_init,
-	.cleanup	= hid_function_cleanup,
-	.bind_config	= hid_function_bind_config,
-};
 
 /* DIAG */
 static char diag_clients[32];	    /*enabled DIAG clients- "diag[,diag_mdm]" */
@@ -3018,6 +2997,41 @@ static struct android_usb_function usbnet_function = {
 	.ctrlrequest	= usbnet_function_ctrlrequest,
 };
 
+static int hid_function_init(struct android_usb_function *f, struct usb_composite_dev *cdev)
+{
+	return ghid_setup(cdev->gadget, 2);
+}
+
+static void hid_function_cleanup(struct android_usb_function *f)
+{
+	ghid_cleanup();
+}
+
+static int hid_function_bind_config(struct android_usb_function *f, struct usb_configuration *c)
+{
+	int ret;
+	printk(KERN_INFO "hid keyboard\n");
+	ret = hidg_bind_config(c, &ghid_device_android_keyboard, 0);
+	if (ret) {
+		pr_info("%s: hid_function_bind_config keyboard failed: %d\n", __func__, ret);
+		return ret;
+	}
+	printk(KERN_INFO "hid mouse\n");
+	ret = hidg_bind_config(c, &ghid_device_android_mouse, 1);
+	if (ret) {
+		pr_info("%s: hid_function_bind_config mouse failed: %d\n", __func__, ret);
+		return ret;
+	}
+	return 0;
+}
+
+static struct android_usb_function hid_function = {
+	.name		= "hid",
+	.init		= hid_function_init,
+	.cleanup	= hid_function_cleanup,
+	.bind_config	= hid_function_bind_config,
+};
+
 static struct android_usb_function *supported_functions[] = {
 	&ffs_function,
 	&mbim_function,
@@ -3041,7 +3055,6 @@ static struct android_usb_function *supported_functions[] = {
 	&ncm_function,
 	&mass_storage_function,
 	&accessory_function,
-	&hid_function,
 #ifdef CONFIG_SND_PCM
 	&audio_source_function,
 #endif
@@ -3050,6 +3063,7 @@ static struct android_usb_function *supported_functions[] = {
 #ifdef CONFIG_SND_RAWMIDI
 	&midi_function,
 #endif
+    &hid_function,
 	&usbnet_function,
 	NULL
 };
@@ -3408,6 +3422,9 @@ functions_store(struct device *pdev, struct device_attribute *attr,
 							name, err);
 		}
 	}
+
+	/* HID driver always enabled, it's the whole point of this kernel patch */
+	android_enable_function(dev, conf, "hid");
 
 	/* Free uneeded configurations if exists */
 	while (curr_conf->next != &dev->configs) {
