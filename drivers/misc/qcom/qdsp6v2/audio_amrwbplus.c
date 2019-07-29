f1829d81b36c56b636424d0e5df1941a8f673c99
Rohit Vashisht <rohitv88cf@gmail.com>
2019-07-29 21:14:16 +0530

    Silence build warnings generated by gcc
    Disable switch-unreachable for cs35l34 codec.
    
    Signed-off-by: Rohit Vashisht <rohitv88cf@gmail.com>

--- drivers/misc/qcom/qdsp6v2/audio_amrwbplus.c
+++ drivers/misc/qcom/qdsp6v2/audio_amrwbplus.c
@@ -60,8 +60,8 @@ static long audio_ioctl_shared(struct file *file, unsigned int cmd,
 		if (audio->feedback == NON_TUNNEL_MODE) {
 			/* Configure PCM output block */
 			rc = q6asm_enc_cfg_blk_pcm(audio->ac,
-			audio->pcm_cfg.sample_rate,
-			audio->pcm_cfg.channel_count);
+				audio->pcm_cfg.sample_rate,
+				audio->pcm_cfg.channel_count);
 			if (rc < 0) {
 				pr_err("pcm output block config failed\n");
 				break;
